7a5006949577f694bbd450f4888e0e0d0d62ca36
nakayama91 <k.naka10ry@gmail.com>
2021-11-24 07:02:38 +0000

    root_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :customers,skip: [:passwords,], controllers: {
+    registrations: "customer/registrations",
+    sessions: 'customer/sessions'
+  }
+
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
+  #customer側↓
+
+  scope module: :customer do
+    #ホーム画面ルーティング
+    root to: 'homes#top'
+    get 'home/about' => 'homes#about'
+
+  end
+
 end
