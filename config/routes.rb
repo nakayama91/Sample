c3d1ffe2e896eefb4d642a5e63261e96531830e0
nakayama91 <k.naka10ry@gmail.com>
2021-11-27 03:50:02 +0000

    finish_routes

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,35 @@ Rails.application.routes.draw do
     root to: 'homes#top'
     get 'home/about' => 'homes#about'
 
+    #カスタマー
+    resource :customers, only: [:edit, :update]
+      get 'customers/my_page' => 'customers#show'
+      get 'customers/leave' => 'customers#leave'
+      patch 'customers/withdraw' => 'customers#withdraw'
+
+    #商品
+    resources :products, only: [:index, :show] do
+      collection do
+        get 'genre_search' => 'products#search'
+      end
+    end
+
+    #配送先
+    resources :shipping_addresses, only: [:index, :create, :edit, :update, :destroy]
+
+    #注文
+    resources :orders, only: [:index, :show, :create, :new] do
+      get 'complete',   on: :collection
+      post 'confirm', on: :collection
+    end
+
+    #カート商品
+    resources :cart_products, only:[:index, :destroy, :edit, :update, :create] do
+      collection do
+        delete 'cart_products/destroy_all' => 'cart_products#destroy_all'
+      end
+    end
+
   end
 
 end
